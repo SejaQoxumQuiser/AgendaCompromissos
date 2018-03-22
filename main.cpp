7390d2e82a75d6274c55b54782e04ccc5493b087
eversonott <everson.o@hotmai.com>
2018-03-22 02:56:41 -0300

    criação da classe nó

--- main.cpp
+++ main.cpp
@@ -4,7 +4,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
+template <typename T>
+class Ldde;
+
+template <typename T>
+class No{
+private:
+    T valor;
+    No * prox;
+    No*  ant;
+public:
+    T getValor() const{
+        return valor;
+    }
+    No(T valor):valor(valor), prox(NULL), ant(NULL) {}
+
+    friend class Ldde<T>;
+};
+
 int main(){
-    cout<<"alterei"<<endl;
+
     return 0;
 }
